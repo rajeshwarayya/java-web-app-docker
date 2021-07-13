25e82ca5d068895a1e2e898ac9d00366436bce55
RAJESHWARAYYA MATHAPATI <74556546+rajeshwarayya@users.noreply.github.com>
2021-07-13 11:45:25 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Dockerrrrr";
+			return "Hello Docker";
 		}else{
 			return "Hello " + name;
 		}
